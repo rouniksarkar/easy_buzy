cb66b48b5ebfc1e5445a7ae5e48cfefc92345856
Rounik Sarkar <rouniksarkar42910@gmail.com>
2026-08-15 02:19:31 +0530

    Frontend and Backend connected through docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
+import { useEffect,useState } from "react"
+
 function App() {
+
+  const [message,setMessage] = useState("");
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/test")
+    .then((res)=>res.json())
+    .then((data)=>setMessage(data.message))
+    .catch((err)=>{
+      console.error("error to message",err)
+    })
+  },[])
 
   return (
     <>
       <h1>New B2B project is building!</h1>
+      <h2>{message}</h2>
     </>
   )
 }
--- server/app.js
+++ server/app.js
@@ -1,9 +1,28 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
 
-app.get("/",(req,res)=>{
+app.use(express.json())
+
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000",
+            // add url or ip
+        ],
+        credentials: true
+    }
+))
+
+app.get("/", (req, res) => {
     res.send("New B2B project is on the way!")
 })
 
-export {app}
+app.get("/test", (req, res) => {
+    res.json({ message: "testing on connectivity on docker." })
+})
+
+export { app }
